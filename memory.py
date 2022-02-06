6ae378cb0abfb113b3e2e69cc676285e1fc501ff
mmadreza99 <mohammad78t@gmail.com>
2022-02-06 06:49:53 +0330

    change img

--- memory.py
+++ memory.py
@@ -16,7 +16,7 @@ GRAY = (211, 211, 211)
 TILE_WIDTH = 100
 TILE_FRONT_COL = BLACK
 
-img1, img2, img3, img4, img5, img6, img7, img8 = '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'
+img1, img2, img3, img4, img5, img6, img7, img8 = '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'
 img_lst = [img1, img2, img3, img4, img5, img6, img7, img8]
 
 
